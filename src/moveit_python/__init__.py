aee13c185ba3d7836133f6f19b8a46da7a55226a
Michael Ferguson <fergs@unboundedrobotics.com>
2014-06-12 19:17:33 -0700

    add mainpage description to docs

--- src/moveit_python/__init__.py
+++ src/moveit_python/__init__.py
@@ -1,3 +1,6 @@
+## @mainpage MoveIt! Python Bindings
+## moveit_python is a set of pure python bindings to MoveIt! using the ROS API.
+
 from planning_scene_interface import PlanningSceneInterface
 from pick_place_interface import PickPlaceInterface
 from move_group_interface import MoveGroupInterface
