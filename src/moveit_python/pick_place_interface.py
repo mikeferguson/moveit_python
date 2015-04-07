587bd062e931f99abb804790cf3e30c1ddd9258c
Michael Ferguson <mfergs7@gmail.com>
2015-04-06 18:38:33 -0700

    implement kwargs for pick&place interface

--- src/moveit_python/pick_place_interface.py
+++ src/moveit_python/pick_place_interface.py
@@ -1,3 +1,4 @@
+# Copyright 2015, Fetch Robotics Inc
 # Copyright 2011-2014, Michael Ferguson
 # All rights reserved.
 #
@@ -60,24 +61,79 @@ class PickPlaceInterface:
     ## @param grasps Grasps to try (moveit_msgs/Grasp)
     ## @param support_name Name of the support surface
     ## @returns moveit_msgs/PickupResult
-    def pickup(self, name, grasps, support_name = "",
-               allow_gripper_support_collision = True,
-               allowed_touch_objects = list()):
+    def pickup(self, name, grasps, **kwargs):
+        # Check arguments
+        supported_args = ("allow_gripper_support_collision",
+                          "allowed_touch_objects",
+                          "plan_only",
+                          "planner_id",
+                          "planning_time",
+                          "support_name")
+        for arg in kwargs.keys():
+            if not arg in supported_args:
+                rospy.loginfo("pickup: unsupported argument: %s" % arg)
+
+        # Create goal
         g = PickupGoal()
+
+        # 1. Fill in target name
         g.target_name = name
+
+        # 2. Fill in group name
         g.group_name = self._group
+
+        # 3. Fill in end effector
         g.end_effector = self._effector
+
+        # 4. List of grasps
         g.possible_grasps = grasps
-        g.support_surface_name = support_name
-        g.allow_gripper_support_collision = allow_gripper_support_collision
-        g.attached_object_touch_links = list() # empty list = use all links of end-effector
-        #g.path_constraints = ??
-        #g.planner_id = ??
-        g.allowed_touch_objects = allowed_touch_objects
-        g.allowed_planning_time = self.allowed_planning_time
+
+        # 5. Fill in support surface
+        try:
+            g.support_surface_name = kwargs["support_name"]
+        except KeyError:
+            g.support_surface_name = ""
+
+        # 6. Can gripper contact the support surface (named above)
+        try:
+            g.allow_gripper_support_collision = kwargs["allow_gripper_support_collision"]
+        except KeyError:
+            g.allow_gripper_support_collision = True
+
+        # 7. What links are part of the "gripper" (and can contact the object/surface)
+        try:
+            g.attached_object_touch_links = kwargs["attached_object_touch_links"]
+        except KeyError:
+            g.attached_object_touch_links = list() # empty list = use all links of end-effector
+
+        # 8. Fill in minimize_object_distance
+
+        # 9. Fill in path_constraints
+
+        # 10. Fill in planner id
+        try:
+            g.planner_id = kwargs["planner_id"]
+        except KeyError:
+            if self.planner_id:
+                g.planner_id = self.planner_id
+
+        # 11. List of obstacles that we are allowed to touch
+        try:
+            g.allowed_touch_objects = kwargs["allowed_touch_objects"]
+        except KeyError:
+            g.allowed_touch_objects = list()
+
+        # 12. Planning time
+        try:
+            g.allowed_planning_time = kwargs["planning_time"]
+        except KeyError:
+            g.allowed_planning_time = self.allowed_planning_time
+
+        # 13. Planning options
         g.planning_options.planning_scene_diff.is_diff = True
         g.planning_options.planning_scene_diff.robot_state.is_diff = True
         g.planning_options.plan_only = self._plan_only
+
         self._pick_action.send_goal(g)
         self._pick_action.wait_for_result()
         return self._pick_action.get_result()
@@ -89,38 +145,93 @@ class PickPlaceInterface:
     ## @param goal_is_eef Set to true if the place goal is for the
     ##        end effector frame, default is object frame.
     ## @returns moveit_msgs/PlaceResult
-    def place(self, name, locations, support_name = "",
-              allow_gripper_support_collision = True,
-              allowed_touch_objects = list(),
-              goal_is_eef = False):
+    def place(self, name, locations, **kwargs):
+        # Check arguments
+        supported_args = ("allow_gripper_support_collision",
+                          "allowed_touch_objects",
+                          "goal_is_eef",
+                          "plan_only",
+                          "planner_id",
+                          "planning_time",
+                          "support_name")
+        for arg in kwargs.keys():
+            if not arg in supported_args:
+                rospy.loginfo("place: unsupported argument: %s" % arg)
+
+        # Create goal
         g = PlaceGoal()
+
+        # 1. Name of ARM planning group
         g.group_name = self._group
+
+        # 2. Name of attached object to place
         g.attached_object_name = name
+
+        # 3. Possible locations to place
         g.place_locations = locations
-        g.place_eef = goal_is_eef
-        g.support_surface_name = support_name
-        g.allow_gripper_support_collision = allow_gripper_support_collision
-        #g.path_constraints = ??
-        #g.planner_id = ??
-        g.allowed_touch_objects = allowed_touch_objects
-        g.allowed_planning_time = self.allowed_planning_time
+
+        # 4. If true, using eef pose (same as pick)
+        try:
+            g.place_eef = kwargs["goal_is_eef"]
+        except KeyError:
+            g.place_eef = False
+
+        # 5. Fill in support surface
+        try:
+            g.support_surface_name = kwargs["support_name"]
+        except KeyError:
+            g.support_surface_name = ""
+
+        # 6. Can gripper contact the support surface (named above)
+        try:
+            g.allow_gripper_support_collision = kwargs["allow_gripper_support_collision"]
+        except KeyError:
+            g.allow_gripper_support_collision = True
+
+        # 7. What links are part of the "gripper" (and can contact the object/surface)
+        try:
+            g.attached_object_touch_links = kwargs["attached_object_touch_links"]
+        except KeyError:
+            g.attached_object_touch_links = list() # empty list = use all links of end-effector
+
+        # 8. Fill in path_constraints
+
+        # 9. Fill in planner id
+        try:
+            g.planner_id = kwargs["planner_id"]
+        except KeyError:
+            if self.planner_id:
+                g.planner_id = self.planner_id
+
+        # 10. List of obstacles that we are allowed to touch
+        try:
+            g.allowed_touch_objects = kwargs["allowed_touch_objects"]
+        except KeyError:
+            g.allowed_touch_objects = list()
+
+        # 11. Planning time
+        try:
+            g.allowed_planning_time = kwargs["planning_time"]
+        except KeyError:
+            g.allowed_planning_time = self.allowed_planning_time
+
+        # 12. Planning options
         g.planning_options.planning_scene_diff.is_diff = True
         g.planning_options.planning_scene_diff.robot_state.is_diff = True
         g.planning_options.plan_only = self._plan_only
+
         self._place_action.send_goal(g)
         self._place_action.wait_for_result()
         return self._place_action.get_result()
 
     ## Common usage pattern
     ## TODO document
-    def pick_with_retry(self, name, grasps, retries=5, scene = None,
-                        support_name = "", allow_gripper_support_collision = True,
-                        allowed_touch_objects = list()):
+    def pick_with_retry(self, name, grasps, retries=5, scene = None, **kwargs):
         if self._verbose:
             rospy.loginfo("Beginning to pick.")
         while retries > 0:
             retries += -1
-            pick_result = self.pickup(name, grasps, support_name=support_name)
+            pick_result = self.pickup(name, grasps, **kwargs)
             if pick_result.error_code.val == MoveItErrorCodes.SUCCESS:
                 rospy.loginfo("Pick succeeded")
                 return [True, pick_result]
@@ -147,15 +258,12 @@ class PickPlaceInterface:
 
     ## Common usage pattern
     ## TODO document
-    def place_with_retry(self, name, locations, retries=5, scene = None,
-                         support_name = "", allow_gripper_support_collision = True,
-                         allowed_touch_objects = list(),
-                         goal_is_eef = False):
+    def place_with_retry(self, name, locations, retries=5, scene = None, **kwargs):
         if self._verbose:
             rospy.loginfo("Beginning to place.")
         while retries > 0:
             retries += -1
-            place_result = self.place(name, locations, support_name, allow_gripper_support_collision, allowed_touch_objects, goal_is_eef)
+            place_result = self.place(name, locations, **kwargs)
             if place_result.error_code.val == MoveItErrorCodes.SUCCESS:
                 rospy.loginfo("Place succeeded")
                 return [True, place_result]
@@ -179,4 +287,3 @@ class PickPlaceInterface:
                 continue
         rospy.logerr("Place failed, and all retries are used")
         return [False, place_result]
-
