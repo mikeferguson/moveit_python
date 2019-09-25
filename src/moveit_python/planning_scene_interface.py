da71746884abaf1a3e22624104af4f0cd3693715
Adwait Naik <adwaitnaik2@gmail.com>
2019-09-25 12:16:23 +0530

    Second Commit

--- src/moveit_python/planning_scene_interface.py
+++ src/moveit_python/planning_scene_interface.py
@@ -237,6 +237,23 @@ class PlanningSceneInterface(object):
 
         self.addSolidPrimitive(name, s, ps.pose, use_service)
 
+    # Add Sphere
+   def addSphere(self, name, radius, x, y, z, use_service=True):
+        
+        s = SolidPrimitive()
+        s.dimensions = [radius]
+        s.type = s.SPHERE
+
+        ps = PoseStamped()
+        ps.header.frame_id = self._fixed_frame
+        ps.pose.position.x = x
+        ps.pose.position.y = y
+        ps.pose.position.z = z
+        ps.pose.orientation.w = 1.0
+
+        self.addSolidPrimitive(name, s, ps.pose, use_service)
+
+         
     ## @brief Insert new box into planning scene
     ## @param name Name of the object
     ## @param size_x The x-dimensions size of the box
