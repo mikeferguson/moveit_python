41e6e4b3bdd34f69646350078e3139096052b983
v4hn <me@v4hn.de>
2020-03-30 16:09:00 +0200

    python3 compatible API & syntax

--- src/moveit_python/__init__.py
+++ src/moveit_python/__init__.py
@@ -1,9 +1,9 @@
 ## @mainpage MoveIt! Python Bindings
 ## moveit_python is a set of pure python bindings to MoveIt! using the ROS API.
 
-from planning_scene_interface import PlanningSceneInterface
-from pick_place_interface import PickPlaceInterface
-from move_group_interface import MoveGroupInterface
-from fake_group_interface import FakeGroupInterface
+from .planning_scene_interface import PlanningSceneInterface
+from .pick_place_interface import PickPlaceInterface
+from .move_group_interface import MoveGroupInterface
+from .fake_group_interface import FakeGroupInterface
 
 __all__ = ["PlanningSceneInterface", "PickPlaceInterface", "MoveGroupInterface", "FakeGroupInterface"]
