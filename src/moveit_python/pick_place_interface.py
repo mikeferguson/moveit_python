b70a7078acd7155fe372b21ef16c1a0440c43bf2
Karl Kangur <karl.kangur@exwzd.com>
2020-04-23 16:38:59 +0900

    Fixed a comparison and logical statement order issue.

--- src/moveit_python/pick_place_interface.py
+++ src/moveit_python/pick_place_interface.py
@@ -260,10 +260,10 @@ class PickPlaceInterface(object):
                 rospy.logerr("Pick failed in the planning stage, try again...")
                 rospy.sleep(0.5)  # short sleep to try and let state settle a bit?
                 continue
-            elif scene and \
-                pick_result.error_code.val == MoveItErrorCodes.CONTROL_FAILED or \
+            elif not scene == None and \
+                (pick_result.error_code.val == MoveItErrorCodes.CONTROL_FAILED or \
                 pick_result.error_code.val == MoveItErrorCodes.MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE or \
-                pick_result.error_code.val == MoveItErrorCodes.TIMED_OUT:
+                pick_result.error_code.val == MoveItErrorCodes.TIMED_OUT):
                 rospy.logerr("Pick failed during execution, attempting to cleanup.")
                 if name in scene.getKnownAttachedObjects():
                     rospy.loginfo("Pick managed to grab object, retreat must have failed, continuing anyways")
